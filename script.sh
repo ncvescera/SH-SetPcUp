651bf968dc49ce1a2eb0369f469475ab942aba13
Nicolò Vescera <slayercandy@icloud.com>
2015-09-20 21:04:16 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,20 +13,25 @@ echo "
 ╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝
 
 "
+
 echo "Start ..."
 apt-get update
 echo "Update Done!"
+
 apt-get upgrade -y
 echo "Upgrade Done!"
+
 echo "Installing git ..."
 apt-get install git -y
 echo "Insall git Done!"
 git config --global user.name "Nicolò Vescera"
 git config --global user.email "slayercandy@icloud.com"
 echo "Git configuration Done!"
+
 echo "Installing Gedit ..."
 apt-get install gedit -y
 echo "Install Gedit Done!"
+
 echo "Installing NetBeans ..."
 wget http://download.oracle.com/otn-pub/java/jdk-nb/8u60-8.0.2/jdk-8u60-nb-8_0_2-linux-x64.sh
 chmod +x jdk-8u60-nb-8_0_2-linux-x64.sh
@@ -41,6 +46,7 @@ case $plugin in
 	   unzip 3380
 	   echo "Pthon Plugin Done!";;
 esac
+
 cd
 cd Documenti/
 mkdir gitdocs
